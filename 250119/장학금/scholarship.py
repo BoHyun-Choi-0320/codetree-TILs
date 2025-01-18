b2d25f7414d264450b3f81c28b2631e69832f774
Choi BoHyun <145964217+BoHyun-Choi-0320@users.noreply.github.com>
2025-01-19 00:30:44 +0900

    Title: 장학금, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250119/장학금/scholarship.py
+++ 250119/장학금/scholarship.py
@@ -1,9 +1,8 @@
 middleScore, finalScore = map(int, input().split())
 
-if middleScore >= 90:
-    if finalScore >= 95 and finalScore <= 100:
-        print('100000')
-    elif finalScore >= 90:
-        print('50000')
+if middleScore >= 90 and finalScore >= 95 and finalScore <= 100:
+    print('100000')
+elif middleScore >= 90 and finalScore >= 90:
+    print('50000')
 else:
     print('0')
