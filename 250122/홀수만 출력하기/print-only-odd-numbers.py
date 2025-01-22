a854fe2439faa0447da2bf2d2c16cc6a0c56bbcc
Choi BoHyun <145964217+BoHyun-Choi-0320@users.noreply.github.com>
2025-01-22 21:06:51 +0900

    Title: 홀수만 출력하기, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250122/홀수만 출력하기/print-only-odd-numbers.py
+++ 250122/홀수만 출력하기/print-only-odd-numbers.py
@@ -1,6 +1,12 @@
 n = int(input())
 
-for i in range(1, n+1):
+# for i in range(1, n+1):
+#     num = int(input())
+#     if num % 2 == 1 and num % 3 == 0:
+#         print(num)
+
+while n >= 1:
     num = int(input())
     if num % 2 == 1 and num % 3 == 0:
         print(num)
+    n-=1
