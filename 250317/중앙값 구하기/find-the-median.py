7be0e60eeb274e04f6018db3b4a6404b6088dc60
Choi BoHyun <145964217+BoHyun-Choi-0320@users.noreply.github.com>
2025-03-17 08:41:13 +0900

    Title: 중앙값 구하기, Time: 54ms, Memory: 16MB, Status: Passed - CodeTree

--- 250317/중앙값 구하기/find-the-median.py
+++ 250317/중앙값 구하기/find-the-median.py
@@ -1,6 +1,19 @@
-list = input().split()
+A, B, C = map(int, input().split())
 
-list.sort()
+if A > B and A > C:
+    if B > C:
+        list = [C, B, A]
+    else:
+        list = [B, C, A]
+elif B > A  and B > C:
+    if A > C:
+        list = [C, A, B]
+    else:
+        list = [A, C, B]
+elif C > A and C > B:
+    if A > B:
+        list = [B, A, C]
+    else:
+        list = [A, B, C]
 
 print(list[1])
-
